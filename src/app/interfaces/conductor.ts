ea0d30fecd09957a9a36b8a7a7051e31e69325f3
ErwinG90 <erw.gonzalez@duocuc.cl>
2023-10-29 15:54:17 -0300

    storage de usuario y conductor + arreglos extras

---
+++ src/app/interfaces/conductor.ts
@@ -0,0 +1,10 @@
+export interface Conductor {
+    nombre:string,
+    apellido:string,
+    fecha_nacimiento:Date,
+    email:string,
+    password:string,
+    tipo_auto:string,
+    matricula:string,
+    asientos:number
+}
--- src/app/pages/registeruser/registeruser.page.ts
+++ src/app/pages/registeruser/registeruser.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-registeruser',
@@ -8,14 +10,59 @@ import { Router } from '@angular/router';
 })
 export class RegisteruserPage implements OnInit {
 
-  constructor(private router:Router) { }
-
-  ngOnInit() {
+  usuario:Usuario={
+    nombre:'',
+    apellido:'',
+    email:'',
+    password:''
   }
 
-  onSubmit(){
-    this.router.navigate(['/tab-inicial/inicio-cliente'])
+  repetirContrasena: string = '';
+
+  storageInitialized: boolean = false; 
+
+  constructor(private router:Router, private storage:Storage) { }
+
+  async ngOnInit() {
+    if (!this.storageInitialized) {
+      await this.storage.create();
+      this.storageInitialized = true;
+    }
   }
+
+  async onSubmit(){
+    if (this.usuario.password === this.repetirContrasena) {
+      let Usuarios: Usuario[] = (await this.storage.get('usuarios')) || [];
+
+      Usuarios.push({
+        nombre: this.usuario.nombre,
+        apellido: this.usuario.apellido,
+        email: this.usuario.email,
+        password: this.usuario.password,
+      });
+
+      await this.storage.set('usuarios', Usuarios);
+
+      await this.obtenerUsuarios();
+      console.log('<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>');
+      console.log("USUARIO ACTUAL = ",this.usuario);
+
+      this.router.navigate(['/tab-inicial/inicio-cliente'])
+
+
+    } else {
+      console.log('Formulario inválido o las contraseñas no coinciden');
+    }
+}
+
+async obtenerUsuarios() {
+  const user = await this.storage.get('usuarios');
+  if (user) {
+    console.log(user);
+  } else {
+    console.log('No se encontraron los USUARIOS PTM');
+  }
+}
 
 
 }
