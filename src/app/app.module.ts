bf9a7d2939ddc2ab520fcadf900360cfc88b489b
Christopher Pineda <apinedachris@gmail.com>
2023-10-29 21:17:02 -0300

    tsautenticar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AngularFireModule} from '@angular/fire/compat';
+import { AngularFireAuthModule} from '@angular/fire/compat/auth';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+    IonicStorageModule.forRoot(), AngularFireModule, AngularFireAuthModule, 
+    AngularFireModule.initializeApp(environment.firebaseConfig)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  
+  firebaseConfig : {
+    apiKey: "AIzaSyAeY1I_hXmV5zE5O239GFVgM-sM53X28ac",
+    authDomain: "viappje.firebaseapp.com",
+    projectId: "viappje",
+    storageBucket: "viappje.appspot.com",
+    messagingSenderId: "909989822685",
+    appId: "1:909989822685:web:c66d153c4618c6891ddb34",
+    measurementId: "G-QKSHQTCTR7"
+  }
 };
 
 /*
